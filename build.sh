cac7902500a90a0b209544e0e2e1cd5cc5d28dfa
Takafumi Yoshida <yoshida.takafumi@classmethod.jp>
2019-10-22 21:04:01 +0900

    modify build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -xeo pipefail
+
 if [[ "$VERSION" = "" ]]; then
     BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
     TAG_BRANCH=${BRANCH_NAME//\//-}
@@ -7,6 +9,6 @@ if [[ "$VERSION" = "" ]]; then
     VERSION="${TAG_BRANCH}-${TAG_DATE}"
 fi
 
-go build -ldflags "-X github.com/zephiransas/go-simple-server/version.version=$VERSION" -o server
+GOOS=linux GOARCH=amd64 go build -ldflags "-X github.com/zephiransas/go-simple-server/version.version=$VERSION" -o server
 ret=$?
 [[ $ret -eq 0 ]] && echo $VERSION
