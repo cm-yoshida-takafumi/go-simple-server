bc5e18576edeec2d3d4506ecfa70fcc898c55055
Takafumi Yoshida <yoshida.takafumi@classmethod.jp>
2019-10-23 17:13:47 +0900

    fix build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,15 @@
 
 set -xeo pipefail
 
-if [[ "$VERSION" = "" ]]; then
-    BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-    TAG_BRANCH=${BRANCH_NAME//\//-}
-    TAG_DATE=$(date '+%Y%m%d-%H%M%S')
-    VERSION="${TAG_BRANCH}-${TAG_DATE}"
-fi
+#if [[ "$VERSION" = "" ]]; then
+#    BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+#    TAG_BRANCH=${BRANCH_NAME//\//-}
+#    TAG_DATE=$(date '+%Y%m%d-%H%M%S')
+#    VERSION="${TAG_BRANCH}-${TAG_DATE}"
+#fi
 
 #GOOS=linux GOARCH=amd64 go build -ldflags "-X github.com/zephiransas/go-simple-server/version.version=$VERSION" -o server
-go build -ldflags "-X github.com/zephiransas/go-simple-server/version.version=$VERSION" -o server
-ret=$?
-[[ $ret -eq 0 ]] && echo $VERSION
+#go build -ldflags "-X github.com/zephiransas/go-simple-server/version.version=$VERSION" -o server
+#ret=$?
+#[[ $ret -eq 0 ]] && echo $VERSION
+go build -o server
