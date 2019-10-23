d499c39145e8adcc2011d84e9f5f32cb87e465b9
Takafumi Yoshida <yoshida.takafumi@classmethod.jp>
2019-10-23 17:49:18 +0900

    build artifacts

--- create_deployment.sh
+++ create_deployment.sh
@@ -2,15 +2,15 @@
 
 set -xeo pipefail
 
-rm *.zip
+#rm *.zip
+#
+#if [[ "$VERSION" = "" ]]; then
+#    BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+#    TAG_BRANCH=${BRANCH_NAME//\//-}
+#    TAG_DATE=$(date '+%Y%m%d-%H%M%S')
+#    VERSION="${TAG_BRANCH}-${TAG_DATE}"
+#fi
 
-if [[ "$VERSION" = "" ]]; then
-    BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-    TAG_BRANCH=${BRANCH_NAME//\//-}
-    TAG_DATE=$(date '+%Y%m%d-%H%M%S')
-    VERSION="${TAG_BRANCH}-${TAG_DATE}"
-fi
+zip go-simple-server appspec.yml server start.sh
 
-zip $VERSION appspec.yml server start.sh
-
-aws s3 cp $VERSION.zip s3://go-simple-server-deployment
+#aws s3 cp $VERSION.zip s3://go-simple-server-deployment
